fb2e0231ce98a5ecd8c525741ad64eb424fff351
sid10t <2698118067@qq.com>
2023-05-06 10:10:11 +0800

    common methods used to test File Programming

---
+++ src/test/java/com/sidiot/netty/c1/TestWalkFileTree.java
@@ -0,0 +1,36 @@
+package com.sidiot.netty.c1;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestWalkFileTree {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("D:\\jdk-17");
+        // 文件目录数目
+        AtomicInteger dirCount = new AtomicInteger();
+        // 文件数目
+        AtomicInteger fileCount = new AtomicInteger();
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("===> " + dir + " <===");
+                // 增加文件目录数
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                // 增加文件数
+                fileCount.incrementAndGet();
+                return super.visitFile(file, attrs);
+            }
+        });
+        // 打印数目
+        System.out.println("文件目录数:"+dirCount.get());
+        System.out.println("文件数:"+fileCount.get());
+    }
+}
