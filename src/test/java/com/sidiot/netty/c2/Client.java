a523b54b45aa195cf260c6886d94bb2f2a166732
sid10t <2698118067@qq.com>
2023-05-09 09:22:42 +0800

    updated something

--- src/test/java/com/sidiot/netty/c2/Client.java
+++ src/test/java/com/sidiot/netty/c2/Client.java
@@ -2,12 +2,14 @@ package com.sidiot.netty.c2;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress("localhost", 7999));
+        SocketAddress address = sc.getLocalAddress();
         System.out.println("waiting...");
     }
 }
