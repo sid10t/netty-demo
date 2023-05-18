279acb5251f70733906d420a34d14450a5342680
sid10t <2698118067@qq.com>
2023-05-18 14:56:18 +0800

    新增可写事件

---
+++ src/test/java/com/sidiot/netty/c2/WriteClient.java
@@ -0,0 +1,21 @@
+package com.sidiot.netty.c2;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class WriteClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel sc = SocketChannel.open();
+        sc.connect(new InetSocketAddress("localhost", 7999));
+
+        int cnt = 0;
+        while (true) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
+            cnt += sc.read(buffer);
+            System.out.println(cnt);
+            buffer.clear();
+        }
+    }
+}
---
+++ src/test/java/com/sidiot/netty/c2/WriteServer.java
@@ -0,0 +1,68 @@
+package com.sidiot.netty.c2;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+public class WriteServer {
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        ssc.bind(new InetSocketAddress(7999));
+
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                iter.remove();
+                if (key.isAcceptable()) {
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    SocketChannel sc = channel.accept();
+                    sc.configureBlocking(false);
+                    SelectionKey scKey = sc.register(selector, 0, null);
+                    scKey.interestOps(SelectionKey.OP_READ);
+
+                    // 1. 向客户端发送大量数据
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 5000000; i++) {
+                        sb.append("sidiot");
+                    }
+                    ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
+
+                    // 2. 返回值代表实际写入的字节数
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+
+                    // 3. 判断是否有剩余内存
+                    if (buffer.hasRemaining()) {
+                        // 4. 关注可写事件 1+4
+                        scKey.interestOps(scKey.interestOps() + SelectionKey.OP_WRITE);
+                        // 5. 将未写完的数据挂到 scKey 上
+                        scKey.attach(buffer);
+                    }
+                } else if (key.isWritable()) {
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    // 6. 清理操作
+                    if (!buffer.hasRemaining()) {
+                        key.attach(null);
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE); // 不在关注写事件
+                    }
+                }
+            }
+        }
+    }
+}
