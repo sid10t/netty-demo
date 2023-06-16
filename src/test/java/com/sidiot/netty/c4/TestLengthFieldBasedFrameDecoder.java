3675e8b3bf79a814d2497dd8a8a89d452a4b7bdb
sid10t <2698118067@qq.com>
2023-06-16 23:17:24 +0800

    更新 LengthFieldBasedFrameDecoder

--- src/test/java/com/sidiot/netty/c4/TestLengthFieldBasedFrameDecoder.java
+++ src/test/java/com/sidiot/netty/c4/TestLengthFieldBasedFrameDecoder.java
@@ -10,16 +10,16 @@ import io.netty.handler.logging.LoggingHandler;
 public class TestLengthFieldBasedFrameDecoder {
     public static void main(String[] args) {
         EmbeddedChannel channel = new EmbeddedChannel(
-                new LengthFieldBasedFrameDecoder(1024, 0, 2, 0, 0),
+                new LengthFieldBasedFrameDecoder(1024, 1, 2, 1, 4),
                 new LoggingHandler(LogLevel.DEBUG)
         );
 
         ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        buffer.writeByte(0xCA);
         buffer.writeBytes(new byte[]{0x00, 0x0c});
+        buffer.writeByte(0xFE);
         buffer.writeBytes("HELLO, WORLD".getBytes());
         channel.writeInbound(buffer);
-
-        System.out.println("\n\n\n\n\n\n\n\n");
     }
 
     private static void send(ByteBuf buf, String content) {
