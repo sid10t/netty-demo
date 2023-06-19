33eb3df32c1f4575364c2eba9910e0e450310c40
sid10t <2698118067@qq.com>
2023-06-19 10:13:15 +0800

    更新 MessageCodec

--- src/main/java/com/sidiot/protocol/MessageCodec.java
+++ src/main/java/com/sidiot/protocol/MessageCodec.java
@@ -10,7 +10,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Arrays;
 import java.util.List;
 
 /**
