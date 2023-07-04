f69cbe72a355e1b37a026371334129067df18276
sid10t <2698118067@qq.com>
2023-07-04 23:33:38 +0800

    新增客户端超时连接测试

---
+++ src/test/java/com/sidiot/netty/c5/TestConnectionTimeout.java
@@ -0,0 +1,30 @@
+package com.sidiot.netty.c5;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestConnectionTimeout {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000)
+                    .channel(NioSocketChannel.class)
+                    .handler(new LoggingHandler());
+            ChannelFuture future = bootstrap.connect("127.0.0.1", 8080);
+            future.sync().channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.debug("TIMEOUT");
+            throw new RuntimeException(e);
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
